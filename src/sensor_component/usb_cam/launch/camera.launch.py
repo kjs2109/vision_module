49232086d977baaa91ae8db1c861524d4fdc593e
kjs2109 <kimjusung2109@naver.com>
2025-03-18 10:36:15 +0900

    add camera2

--- src/sensor_component/usb_cam/launch/camera.launch.py
+++ src/sensor_component/usb_cam/launch/camera.launch.py
@@ -32,35 +32,29 @@
 
 import argparse
 import os
-from pathlib import Path  # noqa: E402
+from pathlib import Path  
 import sys
 
 # Hack to get relative import of .camera_config file working
 dir_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(dir_path)
 
-from camera_config import CameraConfig, USB_CAM_DIR  # noqa: E402
+from camera_config import CameraConfig, USB_CAM_DIR  
 
-from launch import LaunchDescription  # noqa: E402
-from launch.actions import GroupAction  # noqa: E402
-from launch_ros.actions import Node  # noqa: E402
+from launch import LaunchDescription  
+from launch.actions import GroupAction  
+from launch_ros.actions import Node  
 
 
-CAMERAS = []
-CAMERAS.append(
-    CameraConfig(
-        name='camera1',
-        param_path=Path(USB_CAM_DIR, 'config', 'params_1.yaml')
-    )
-    # Add more Camera's here and they will automatically be launched below
-)
+CAMERAS = [
+    CameraConfig(name='camera1', param_path=Path(USB_CAM_DIR, 'config', 'params_1.yaml')),
+    CameraConfig(name='camera2', param_path=Path(USB_CAM_DIR, 'config', 'params_2.yaml')),
+]
 
 
 def generate_launch_description():
     ld = LaunchDescription()
 
-    parser = argparse.ArgumentParser(description='usb_cam demo')
-    parser.add_argument('-n', '--node-name', dest='node_name', type=str, help='name for device', default='usb_cam')
     camera_nodes = [
         Node(
             package='usb_cam', executable='usb_cam_node_exe', output='screen',
